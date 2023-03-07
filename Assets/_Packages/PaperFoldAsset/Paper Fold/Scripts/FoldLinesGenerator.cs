2ba82aa021a4f9488250a873f0c5a0126449c3b4
Vanilka <vfeodorovm@yandex.com>
2023-03-08 00:01:13 +0400

    Changed game field size for Yandex moderation

--- Assets/_Packages/PaperFoldAsset/Paper Fold/Scripts/FoldLinesGenerator.cs
+++ Assets/_Packages/PaperFoldAsset/Paper Fold/Scripts/FoldLinesGenerator.cs
@@ -20,18 +20,6 @@ public class FoldLinesGenerator : MonoBehaviour
         LevelManager.onPaperInstantiated -= DrawFoldingLines;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void ClearPaperDelegates(int none)
     {
         currentPaper.onPaperStateChanged -= UpdateFoldingLines;
